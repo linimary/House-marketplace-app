674558b2f45c683bc2047dfe658abd42fb44ed4b
linimary <mariya.lichanova@gmail.com>
2023-06-01 23:05:22 +0300

    Fixed issues in Listing Item and Create Listing

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -4,6 +4,7 @@ import bedIcon from "../assets/svg/bedIcon.svg";
 import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
 export default function ListingItem({ listing, id, onDelete }) {
+console.log(listing);
   return (
     <li className="categoryListing">
       <Link
@@ -11,7 +12,7 @@ export default function ListingItem({ listing, id, onDelete }) {
         className="categoryListingLink"
       >
         <img
-          src={listing.imageUrls[0]}
+          src={listing.imageUrls}
           alt={listing.name}
           className="categoryListingImg"
         />
